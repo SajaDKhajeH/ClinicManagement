e771270336631e78ce36fbf9cba632e3a4eda5ef
Tikrack <tikrackcode@gmail.com>
2026-06-22 17:53:52 +0330

    feat(adding validation): finish adding validation to patient add field

--- ClinicManagement/FrmPatient.cs
+++ ClinicManagement/FrmPatient.cs
@@ -5,6 +5,8 @@ namespace ClinicManagement
 {
     public partial class FrmAddPatient : Form
     {
+        PatientManager patientManager = new PatientManager();
+
         public FrmAddPatient()
         {
             InitializeComponent();
@@ -17,15 +19,32 @@ namespace ClinicManagement
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            string[] errors = patientManager.PatientValidation(
+                firstName: txtName.Text,
+                lastName: txtLastName.Text,
+                nationalCode: txtNationalCode.Text,
+                mobileNumber: txtMobileNumber.Text
+            );
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(
+                    errors[0],
+                    "Validation Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+
+                return;
+            }
+
             Patient Patient = new Patient();
             Patient.FirstName = txtName.Text;
             Patient.LastName = txtLastName.Text;
             Patient.NationalCode = txtNationalCode.Text;
             Patient.MobileNumber = txtMobileNumber.Text;
 
-            PatientManager PatientManager = new PatientManager();
-            PatientManager.AddPatient(Patient);
+            patientManager.AddPatient(Patient);
         }
-
     }
 }
--- ClinicManagement/PatientManager.cs
+++ ClinicManagement/PatientManager.cs
@@ -28,22 +28,25 @@ namespace ClinicManagement
             List<string> errors = new List<string>();
 
             if (firstName == null || firstName.Trim() == "")
-                errors.Add("First name field is required.");
+                errors.Add("نام اجباری است");
 
             if (lastName == null || lastName.Trim() == "")
-                errors.Add("Last name field is required.");
+                errors.Add("نام خانوادگی اجباری است");
 
             if (nationalCode == null || nationalCode.Trim() == "")
-                errors.Add("National code field is required.");
+                errors.Add("کد ملی اجباری است");
+            else
+            if (!int.TryParse(nationalCode, out int _))
+                errors.Add("کد ملی نامعتبر است");
             else
             if (nationalCode.Length != 10)
-                errors.Add("National code is not valid.");
+                errors.Add("کد ملی باید ۱۰ رقم باشد");
 
             if (mobileNumber == null || mobileNumber.Trim() == "")
-                errors.Add("Mobile field is required.");
+                errors.Add("شماره تلفن اجباری است");
             else
             if (mobileNumber.Length != 11)
-                errors.Add("Mobile number is not valid.");
+                errors.Add("شماره تلفن باید ۱۱ رقم باشد");
 
             return errors.ToArray();
         }
