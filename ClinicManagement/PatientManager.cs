86a0de3decd6c44d0ef42b0e5d869de874589e69
Tikrack <tikrackcode@gmail.com>
2026-06-22 17:36:46 +0330

    adding validation method to the patient service manager

--- ClinicManagement/PatientManager.cs
+++ ClinicManagement/PatientManager.cs
@@ -18,5 +18,34 @@ namespace ClinicManagement
 
             Patients.Add(Patient);
         }
+
+        public string[] PatientValidation(
+            string firstName,
+            string lastName,
+            string nationalCode,
+            string mobileNumber
+        ){
+            List<string> errors = new List<string>();
+
+            if (firstName == null || firstName.Trim() == "")
+                errors.Add("First name field is required.");
+
+            if (lastName == null || lastName.Trim() == "")
+                errors.Add("Last name field is required.");
+
+            if (nationalCode == null || nationalCode.Trim() == "")
+                errors.Add("National code field is required.");
+            else
+            if (nationalCode.Length != 10)
+                errors.Add("National code is not valid.");
+
+            if (mobileNumber == null || mobileNumber.Trim() == "")
+                errors.Add("Mobile field is required.");
+            else
+            if (mobileNumber.Length != 11)
+                errors.Add("Mobile number is not valid.");
+
+            return errors.ToArray();
+        }
     }
 }
