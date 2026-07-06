c6cfc03778adafb650c19751eff22fe55cd73d47
Tikrack <tikrackcode@gmail.com>
2026-07-06 09:06:48 +0330

    Add starter form

---
+++ ClinicManagement/FrmStarter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicManagement
+{
+    public partial class FrmStarter : Form
+    {
+        public FrmStarter()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var frm = new FrmPatients();
+
+            frm.Show();
+        }
+    }
+}
